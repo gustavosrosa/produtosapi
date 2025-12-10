395c6c8f80dfe2a432daaff5b63a2edfb23bc2ba
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-10 20:24:46 -0300

    FEAT: Module1 - Adding initial structure

---
+++ src/test/java/br/com/gustavorosa/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavorosa.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
