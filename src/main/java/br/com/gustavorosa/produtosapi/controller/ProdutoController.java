37ae141c7c0764e5013405529c04ccce9a361486
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-11 15:02:49 -0300

    FEAT: Create repository new product

--- src/main/java/br/com/gustavorosa/produtosapi/controller/ProdutoController.java
+++ src/main/java/br/com/gustavorosa/produtosapi/controller/ProdutoController.java
@@ -1,19 +1,33 @@
 package br.com.gustavorosa.produtosapi.controller;
 
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gustavorosa.produtosapi.model.Produto;
+import br.com.gustavorosa.produtosapi.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 	
+	private ProdutoRepository produtoRepository;
+	
+	public ProdutoController(ProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+
 	@PostMapping
-	public void salvar(@RequestBody Produto produto) {
+	public Produto salvar(@RequestBody Produto produto) {
+	
+		String id = UUID.randomUUID().toString();
+		produto.setId(id);
 		
+		produtoRepository.save(produto);
+		return produto;
 	}
 
 }
--- src/main/java/br/com/gustavorosa/produtosapi/model/Produto.java
+++ src/main/java/br/com/gustavorosa/produtosapi/model/Produto.java
@@ -2,13 +2,19 @@ package br.com.gustavorosa.produtosapi.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "produto")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Produto {
 	
 	@Column
