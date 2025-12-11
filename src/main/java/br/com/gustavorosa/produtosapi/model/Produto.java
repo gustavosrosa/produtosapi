9b06ff8593edd591d8dbb84b34a17f7bc1cf475a
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-11 14:45:50 -0300

    FEAT: Adding entity Produto

--- src/main/java/br/com/gustavorosa/produtosapi/model/Produto.java
+++ src/main/java/br/com/gustavorosa/produtosapi/model/Produto.java
@@ -1,10 +1,27 @@
 package br.com.gustavorosa.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 	
+	@Column
+	@Id
 	private String id;
+	
+	@Column
 	private String nome;
+	
+	@Column
 	private String descricao;
+	
+	@Column
 	private Double preco;
 	
 }
