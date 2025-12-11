e318105fa2d1cd6462e7a9134b87649b8a92fbef
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-12-11 15:10:11 -0300

    FEAT: Creating endpoint get a product by id

--- src/main/java/br/com/gustavorosa/produtosapi/controller/ProdutoController.java
+++ src/main/java/br/com/gustavorosa/produtosapi/controller/ProdutoController.java
@@ -1,7 +1,10 @@
 package br.com.gustavorosa.produtosapi.controller;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,13 @@ public class ProdutoController {
 		
 		produtoRepository.save(produto);
 		return produto;
+	}
+	
+	@GetMapping("{id}")
+	public Produto obterPorId(@PathVariable String id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		return produto.orElse(null);
+		
 	}
 
 }
